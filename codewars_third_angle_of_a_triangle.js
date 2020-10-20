260115c765841ff1808a7c012b19b2d95c5258ec
Crew Spence <csspence123@gmail.com>
2020-10-20 04:53:01 -0500

    Wrote working solution.

--- codewars_third_angle_of_a_triangle.js
+++ codewars_third_angle_of_a_triangle.js
@@ -6,6 +6,6 @@ Write a function to return the 3rd.
 Note: only positive integers will be tested.
 */
 
-function otherAngle(a, b) {
-  return 0;
+const otherAngle = (a, b) => {
+  return 180 - (a + b);
 }
